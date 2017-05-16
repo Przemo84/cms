acf23558e7c73424dad7a50d2149d68c2ade06e0
Your Name <you@example.com>
2017-05-16 14:58:43 +0200

    Laravel - poczatek. Skonfigurowany

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Http\Response;
+
+class WebController extends Controller
+{
+    public function listAll()
+    {
+        return Response("du yt");
+    }
+}
